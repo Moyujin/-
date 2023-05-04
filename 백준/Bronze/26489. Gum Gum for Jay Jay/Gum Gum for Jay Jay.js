1536e66c6c2b6a6f869fd3f8de89a59e8006e161
모유진 <34415392+reunal@users.noreply.github.com>
2023-05-04 23:09:48 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 124 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let stdin = fs.readFileSync(filePath).toString().trim().split('\n');
+console.log(stdin.length)
