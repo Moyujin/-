13941f711eb5b6ee6df6bbd02f744f60d7ed90bb
모유진 <34415392+reunal@users.noreply.github.com>
2023-05-06 16:20:03 +0900

    [level 2] Title: 올바른 괄호, Time: 5.36 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,18 @@
 function solution(s){
-    let stack = 0;
-    
-    if (s[0] == ")" || s.length % 2 !== 0 || s[s.length - 1] === "(") return false;
-     
+    let openBracket = 0
+
     for (const char of s) {
-        char === '(' ? stack++ : stack--;
-        if (stack < 0) {
-            return false;
+        // 열린 괄호를 만났다면 수량을 1개 추가하고 다음 반복을 진행.
+        if (char == '(') {
+            openBracket += 1;
+            continue
         }
+        // 만약 열린 괄호가 없는데 닫힌 괄호가 들어왔다면 false.
+        if (!openBracket) {
+            return false
+        }
+        openBracket -= 1;
     }
-    return !stack;
+    // 괄호가 완전히 열리고 닫혀 stack 내부가 비었다면 true, 아니라면 false
+    return openBracket === 0;
 }
